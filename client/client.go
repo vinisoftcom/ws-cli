383376ded3adaa4d76922861a010f8779443faf0
Peter Stovka <stovka.peter@gmail.com>
2019-09-22 00:40:43 +0200

    add rest for getting current user

--- client/client.go
+++ client/client.go
@@ -6,6 +6,13 @@ import (
 	"path"
 	"encoding/json"
 	"io/ioutil"
+	"fmt"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/base64"
+	"time"
+	"net/http"
 )
 
 type account struct {
@@ -13,10 +20,7 @@ type account struct {
 	Secret string `json:"secret"`
 }
 
-type client struct {
-	id string
-	secret string
-}
+type client struct {  }
 
 func (c client) getConfigFilePath() string {
 	user, err := user.Current()
@@ -108,7 +112,64 @@ func (c client) Logout() {
 	}
 }
 
+// func (c client) Test() {
+	// var sig = c.getSignature("GET", "/v1/user/self", 123)
+	// println(sig)
+// }
+
+func (c client) getSignature(method string, path string, time int64, key string) string {
+	var req = c.getCanonicalRequest(method, path, time)
+	res := hmac.New(sha1.New, []byte(key))
+	res.Write([]byte(req))
+	sha := hex.EncodeToString(res.Sum(nil))
+	return sha
+}
+
+func (c client) getCanonicalRequest(method string, path string, time int64) string {
+	return fmt.Sprintf("%s %s %d", method, path, time)
+}
+
+func (c client) CurrentUser() (string, error) {
+	var account, _ = c.getAccountConfig()
+
+	location,_ := time.LoadLocation("GMT")
+	var now = time.Now().In(location)
+	var time = now.UnixNano() / int64(time.Second)
+	var method = "GET"
+	var path = "/v1/user/self"
+	var signature = c.getSignature(method, path, time, account.Secret)
+
+	var auth = fmt.Sprintf("%s:%s", account.Id, signature)
+	basic :=  base64.StdEncoding.EncodeToString([]byte(auth))
+
+	var url = fmt.Sprintf("https://rest.websupport.sk%s", path)
+
+	httpClient := &http.Client{}
+
+	req, err := http.NewRequest(method, url, nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", basic))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Date", now.Format("20060102T150405Z"))
+
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
 func GetClient() *client {
-	client := client{id: "id", secret: "secret"}
+	client := client{}
 	return &client
 }
